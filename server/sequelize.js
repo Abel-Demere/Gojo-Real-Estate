a7e0b801f013c6a3ad98652414d4a33a040deb79
Abel-Demere <abell.demere@gmail.com>
2024-05-21 19:34:02 +0300

    update

--- server/sequelize.js
+++ server/sequelize.js
@@ -1,9 +1,13 @@
-// sequelize.js
 const { Sequelize } = require("sequelize");
+const pg = require("pg"); // Import pg explicitly
+
+// Set the pg library to be used by Sequelize
+pg.defaults.parseInt8 = true;
 
 const sequelize = new Sequelize("rent", "postgres", "1230123", {
   host: "localhost",
   dialect: "postgres",
+  dialectModule: pg, // Specify the imported pg module
   define: {
     timestamps: false, // Disable timestamps for simplicity
   },
